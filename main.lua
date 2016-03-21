0e1ff2294aa58b43d2b6a7954a316bc7ebf15445
harrylclc <cul226@psu.edu>
2016-03-21 14:06:43 -0400

    update gpu

--- main.lua
+++ main.lua
@@ -1,12 +1,11 @@
 require 'hdf5'
-require 'cutorch'
 require 'rnn'
-require 'cunn'
-require 'cudnn'
 require 'optim'
 
 cmd = torch.CmdLine()
 cmd:option('-data', '/data_giles/cul226/simple/preprocessed/newsla_Google.hdf5', 'training data and word2vec data')
+cmd:option('-gpu', 0, 'use gpu')
+cmd:option('-gpuid', 1, 'gpu id')
 
 function load_data()
     local w2v
@@ -40,11 +39,16 @@ opt.niter = 1
 opt.learningRate = 0.001
 opt.momentum = 0.9
 
+if opt.gpu == 1 then
+    require 'cutorch'
+    cutorch.setDevice(opt.gpuid + 1)
+    require 'cunn'
+    require 'cudnn'
+end
+
 local w2v
 local dataX, dataY
 dataX, dataY, w2v = load_data()
---dataX = dataX:cuda()
-dataY = dataY:cuda()
 
 opt.vocabSize = w2v:size(1)
 opt.vecSize = w2v:size(2)
@@ -55,15 +59,19 @@ local dataXRev = torch.Tensor(dataX:size())
 for i = 1, dataX:size(2) do
     dataXRev:select(2, i):copy(dataX:select(2, dataX:size(2) + 1 - i))
 end
-dataXRev = dataXRev:cuda()
 
 -- shift output sequences
 local dataYShift = torch.cat(dataY:select(2, dataY:size(2)), dataY:sub(1, -1, 1, -2), 2)
-dataYShitf = dataYShift:cuda()
+
+if opt.gpu == 1 then
+    dataY = dataY:float():cuda()
+    dataXRev = dataXRev:float():cuda()
+    dataYShitf = dataYShift:float():cuda()
+end
 
 -- pre-trained w2v
 local lookup = nn.LookupTable(opt.vocabSize, opt.vecSize)
-w2v = w2v:cuda()
+-- w2v = w2v:cuda()
 lookup.weight:copy(w2v)
 lookup.weight[1]:zero()
 
@@ -84,12 +92,8 @@ dec:add(nn.Sequencer(decLSTM))
 dec:add(nn.Sequencer(nn.Linear(opt.vecSize, opt.vocabSize)))
 dec:add(nn.Sequencer(nn.LogSoftMax()))
 
-model = nn.Container():add(enc):add(dec)
-
+local model = nn.Container():add(enc):add(dec)
 local criterion = nn.SequencerCriterion(nn.ClassNLLCriterion())
-
-model:cuda()
-criterion:cuda()
 
 params, gradParams = model:getParameters()
 
@@ -97,12 +101,21 @@ params, gradParams = model:getParameters()
 local trainSize = dataX:size(1)
 local numBatches = math.floor(trainSize / opt.batchSize)
 
-sgdState = {learningRate = opt.learningRate,
-            momentum = opt.momentum,
-            learningRateDecay = 5e-7
-           }
+sgdState = {
+    learningRate = opt.learningRate,
+    momentum = opt.momentum,
+    learningRateDecay = 5e-7
+}
 
-local toTable = nn.SplitTable(1, 1):cuda()
+local toTable = nn.SplitTable(1, 1)
+local zeroTensor = torch.Tensor(opt.batchSize, opt.vecSize):zero()
+
+if opt.gpu == 1 then
+    model:cuda()
+    criterion:cuda()
+    toTable:cuda()
+    zeroTensor = zeroTensor:cuda()
+end
 
 for i = 1, opt.niter do
     print('Iter: ' .. i)
@@ -111,18 +124,20 @@ for i = 1, opt.niter do
         print(j)
         local st = (shuffle[j] - 1) * opt.batchSize + 1
         local batchSize = math.min(opt.batchSize, trainSize - st + 1)
+        print('start:', st, 'batchSize', batchSize)
         print('before narrow')
         local encInSeq = dataXRev:narrow(1, st, batchSize)
         local decInSeq = dataYShift:narrow(1, st, batchSize)
         local decOutSeq = dataY:narrow(1, st, batchSize)
         print('after narrow')
-        encInSeq = encInSeq:cuda()
-        decInSeq = decInSeq:cuda()
-        decOutSeq = decOutSeq:cuda()
+        --if opt.gpu == 1 then
+        --    encInSeq = torch.Tensor(batchSize, encInSeq:size(2)):copy(encInSeq):float():cuda()
+        --    decInSeq = decInSeq:float():cuda()
+        --    decOutSeq = decOutSeq:float():cuda()
+        --end
         print('before totable')
         decOutSeq = toTable:forward(decOutSeq)
         print('after totable')
-
 
         local feval = function(x)
             print('before gc')
@@ -139,13 +154,14 @@ for i = 1, opt.niter do
             forwardConnect(encLSTM, decLSTM)
             local decOut = dec:forward(decInSeq)
             print('forward done')
+
             local err = criterion:forward(decOut, decOutSeq)
             print(string.format("NLL err = %f ", err))
+
             -- backward
             local gradOutput = criterion:backward(decOut, decOutSeq)
             dec:backward(decInSeq, gradOutput)
             backwardConnect(encLSTM, decLSTM)
-            local zeroTensor = torch.Tensor(opt.batchSize, opt.vecSize):zero():cuda()
             enc:backward(encInSeq, zeroTensor)
 
             return err, gradParams
